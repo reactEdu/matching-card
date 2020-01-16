a48b301d8e9b872cef9ea951432099b2d4f86d9a
macbook_of_L <ds2lvg@gmail.com>
2020-01-16 12:00:46 +0900

    Updates

---
+++ precache-manifest.7e4cc80ac4997dcde98de543e1078898.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "163d128b2f3b9909e065a27ac48ec084",
+    "url": "/matching-card/index.html"
+  },
+  {
+    "revision": "9e62244c10077e65ee2e",
+    "url": "/matching-card/static/css/main.c301cce8.chunk.css"
+  },
+  {
+    "revision": "7135e49ab10e1890f666",
+    "url": "/matching-card/static/js/2.3087e39b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/matching-card/static/js/2.3087e39b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9e62244c10077e65ee2e",
+    "url": "/matching-card/static/js/main.4dfa9822.chunk.js"
+  },
+  {
+    "revision": "01453b635c55ac645e41",
+    "url": "/matching-card/static/js/runtime-main.aa61efbe.js"
+  }
+]);
